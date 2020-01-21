f5f9f1ce499175be049d39c0aecd057c8c5cf033
AngeloNekko <AngeloNekko@users.noreply.github.com>
2020-01-20 21:49:06 -0500

    Deber

---
+++ Deber/src/MVC/MVC_1.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MVC;
+
+import Modelo.persona;
+import java.util.Scanner;
+
+
+/**
+ *
+ * @author crist
+ */
+public class MVC_1 {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String res;
+        String cedula;
+        do{
+        System.out.println("Coloque el numero de cedula");
+        cedula=sc.next();
+        if(cedula.length()>10 ||cedula.length()<10){
+            System.err.println("la cedula ingresada esta mal porfavor coloque bien los valores");
+            }else{
+            int cedulap=Integer.parseInt(cedula);
+            System.out.print("la cedula es: "+cedulap);
+            }
+            System.out.println("Desea intentarlo de nuevo o salir? n=salir o s=Mostrar resultado");
+        res=sc.next();
+        }while(!res.equalsIgnoreCase("n"));
+        
+        persona cristian = new persona ("cristian","cordova",0, 0,0, "la 41", 'm', true);
+        
+        System.out.println(cristian);
+    }
+}
