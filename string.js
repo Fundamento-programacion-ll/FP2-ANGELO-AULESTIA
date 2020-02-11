a75e21a3a92fcc2a221abc370ae2fe0acfcad419
AngeloNekko <AngeloNekko@users.noreply.github.com>
2020-02-11 09:57:28 -0500

    Update string.js

--- string.js
+++ string.js
@@ -30,5 +30,6 @@ console.log(ejemtri.toUpperCase());
 var noticia = 'Realiza fotografías de 20 megapíxeles y puede utilizar un perfil de color Dlog-M de 10 bits, con el que se obtiene un rango dinámico más amplio al registrar hasta mil millones de colores. Esto queda bastante por encima de los 16 millones de colores de los perfiles de 8 bits, pudiendo dejar más margen y libertad en la edición posterior del material. Por su parte, el Mavic 2 Zoom cuenta con un sensor CMOS de 1/2,3 pulgadas y zoom óptico, lo cual da nombre al propio producto. En este caso es capaz de realizar capturas de 12 megapíxeles, disponiendo un zoom automático híbrido (que combina detección de fase y contraste) de una función de "Superresolución" que recurre al zoom óptico para realizar nueve fotografías y componer una sola con todas ellas de 48 megapíxeles (con más detalle).'
 console.log(noticia.length);
 var noti1=noticia.replace(/ /g,"*-*");
-console.log(noti1.charAt(","));
+console.log(noti1.charAt(",",".",";"));
+console.log(ejemtri.toUpperCase(0,312,790));
 console.log(noti1)
